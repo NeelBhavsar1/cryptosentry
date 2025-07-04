2770e002faa282b9e1c1140d8b461038b24f7bf7
Neel Bhavsar <neelbhavsarr1@gmail.com>
2025-07-04 14:20:06 +0100

    modified website theme

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 import logo_icon from '../../assets/logo1.png'
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
-  return (
-    <div className="navbar-container">
-        <div className="image">
-            <img src={logo_icon} alt="logo of app" />
+    const [selectedCurrency, setSelectedCurrency] = useState('Currency')
+
+    const handleCurrencySelect = (currency) => {
+        setSelectedCurrency(currency)
+        //sort the currency later
+    }
+
+    return (
+        <div className="navbar-container">
+            <div className="image">
+                <img src={logo_icon} alt="logo of app" />
+                {/* SORT THE LOGO OUT LATER */}
+            </div>
+            <div className="navlinks">
+                <ul>
+                    <NavLink to='/'><li>Home</li></NavLink>
+                    <NavLink to='/about'><li>About</li></NavLink>
+                    <NavLink to='/crypto'><li>Crypto</li></NavLink>
+                    <NavLink to='/cryptobot'><li>Talk to Ai</li></NavLink>
+                    <NavLink to='/resources'><li>Resources</li></NavLink>
+                </ul>
+            </div>
+            <div className="actions-btns">
+                <div className="dropdown">
+                    <button className="dropbtn">{selectedCurrency} ▼</button>
+                    <div className="dropdown-content">
+                        <ul>
+                            <li onClick={() => handleCurrencySelect('USD')}>USD</li>
+                            <li onClick={() => handleCurrencySelect('GBP')}>GBP</li>
+                            <li onClick={() => handleCurrencySelect('EUR')}>EUR</li>
+                        </ul>
+                    </div>
+                </div>
+                <button>Sign in</button>
+            </div>
         </div>
-        <div className="navlinks">
-            <ul>
-                <NavLink to='/'><li>Home</li></NavLink>
-                <NavLink to='/about'><li>About</li></NavLink>
-                <NavLink to='/crypto'><li>Crypto</li></NavLink>
-                <NavLink to='/cryptobot'><li>Talk to Ai</li></NavLink>
-                <NavLink to='/resources'><li>Resources</li></NavLink>
-            </ul>
-        </div>
-        <div className="actions-btns">
-            <button>Currency</button>
-            <button>Sign in</button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default NavBar
