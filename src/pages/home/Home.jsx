95eabdd2e18ea50008d35f9a15980b747c0a7f0a
Neel Bhavsar <neelbhavsarr1@gmail.com>
2025-07-04 15:28:34 +0100

    homepage setup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Home.css'
+import app_icon from '../../assets/homeapp.png'
+import { motion } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className="home-container">
-      <h1>Home</h1>
+      <div className="left-container">
+        <h1>Welcome to <br /> Crypto Sentry</h1>
+        <p>
+          Stay ahead in the crypto world by checking cryptocurrency prices in real-time, exploring detailed coin information, and tracking market trends effortlessly.  
+          Get instant insights and analysis by chatting with our AI assistant to understand the latest market movements and make informed decisions.  
+          Whether you’re a beginner or an expert, Crypto Sentry is your go-to platform for all things crypto.
+        </p>
+        <div className="buttons">
+          <button onClick={() => navigate('/crypto')}>View Crypto</button>
+          <button onClick={() => navigate('/resources')}>Resources</button>
+        </div>
+      </div>
+      <div className="right-container">
+        <img src={app_icon} alt="" />
+
+      </div>
       
     </div>
   )
